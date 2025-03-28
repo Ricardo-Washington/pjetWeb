2665649c808d621228de0fd6ff6a8909b6b420c8
Ricardo Washington Rodrigues de souza <r.washington.r.s@gmail.com>
2025-03-28 20:44:29 -0300

    atualização

--- projet/js/login.js
+++ projet/js/login.js
@@ -2,65 +2,60 @@ function onChangeEmail() {
     toggleButtonsDisable();
     toggleEmailErrors();
 }
-function onChangePassword(){
+
+function onChangePassword() {
+    toggleButtonsDisable();
     togglePasswordErrors();
-    toggleButtonsDisable();
-    toggleButtonsDisable();
+} 
+
+function toggleEmailErrors() {
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ?  "none" : "block";
+
+    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
+    
+}
+
+function togglePasswordErrors() {
+    const password = form.password().value;
+    form.passwordRequiredError().style.display = password ? "none" : "block";
+    
+}
+
+function toggleButtonsDisable() {
+    const emailValid = isEmailValid();
+    form.recoverPasswordButton().disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    form.loginButton().disabled = !emailValid || !passwordValid;
 }
 
 function isEmailValid() {
     const email = form.email().value;
-    if(!email){
+    if (!email) {
         return false;
     }
     return validateEmail(email);
 }
 
 function isPasswordValid() {
-    return form.password().value ? true : false;
+    const password = form.password().value;
+    if (!password) {
+        return false;
+    }
+    return true;
 }
 
-function toggleEmailErrors(){
-    const email = form.email().value;
-    form.emailRequiredError().style.display = email ? "none" : "block";
-
-
-    form.emailInvalidError().style.display = validateEmail(email) ? "none": "block";
-   
-
-}
-function togglePasswordErrors(){
-    const password = form.passwoerd().value;
-    form.passwordRequiredError().style.display = password ? "none" : "block";
-}
-
-function toggleButtonsDisable(){
-    const emailValid = isEmailValid();
-    form.recoverPasswordButton().disabled = !emailValid;
-
-    const passwoerdValid =  isPasswordValid();
-    form.loginButton().disabled = !emailValid || !passwoerdValid;
-}
-
-function validateEmail(email){
-    return /\S+@\S+\.\S+/.test(email);
-    // função achada na internet 
-    // sem galantia, clompo puque quiz!
-} 
 
 const form = {
-    email: () => document.getElementById('email'),
-    emailInvalidError: () => document.getElementById('email-invalid-error'),
-    emailRequiredError: () => document.getElementById('email-required-error'),
+    email: () => document.getElementById("email"),
+    emailRequiredError: () => document.getElementById("email-required-error"),
+    emailInvalidError: () => document.getElementById("email-invalid-error"),
 
+    loginButton: () => document.getElementById("login-button"),
 
+    password: () => document.getElementById("password"),
+    passwordRequiredError: () => document.getElementById("password-required-error"),
+    recoverPasswordButton: () => document.getElementById("recover-password-button")
 
-    loginButton: () => document.getElementById('login-button'),
-
-
-    passwoerd: () => document.getElementById('password'),
-    recoverPasswordButton: () => document.getElementById("recover-password-button"),
-    passwordRequiredError: () =>document.getElementById('password-required-error'),
-    
 }
-
